46169644e31f56036ca7d08d0dcb4295d5225125
Lorenanicole <me@lorenamesa.com>
2014-02-13 12:04:15 -0600

    added pirate functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,13 @@
+require 'open-uri'
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
 
-
+post '/pirate' do
+  encoded_uri = URI::encode(params[:url])
+  translation = open(encoded_uri)
+  content_type :json
+  {translation: translation, original: params[:sentence]}.to_json
+end
--- public/js/application.js
+++ public/js/application.js
@@ -8,7 +8,21 @@ $(document).ready(function() {
     $.post('/create_event', {name: name, location: location, starts: starts, ends: ends}, function(response){
       console.log(response);
       var string = "<li>Thing: " + response.url + response.name + "</a> To be held on " + response.starts + "</li>";
-      $("#events").append(string)
-    })
-  })
+       $('#response').empty();
+       $('#response').append("Congrats! You have successfully made that thing " +  response.name + " to now do with people!");
+      $("#events").append(string);
+    });
+  });
+
+  $('#pirate-submit').on('click', function(e){
+      e.preventDefault();
+      var sentence = $('#pirate-sentence').val();
+      var url = 'http://isithackday.com/arrpi.php?text=' + sentence;
+      console.log(url);
+    $.post('/pirate', { url: url }, function(response) {
+      console.log(response);
+      $('#pirate-response').empty();
+      $('#pirate-response').append("<h2> So what say yeeeee as a pirate?!?!</h2><p>" + response.translation + "</p>");
+    });
+  });
 });
